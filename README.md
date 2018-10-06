10e1a0a4fdcd6e7a93b190b9be437f5b8413e957
Matías Danel, Vico <matias.daniel.vico@gmail.com>
2018-10-06 16:27:39 -0300

    Included terminal arguments parsing. Included requisites and usage in the README.

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # WaveformViewer
 Waveform viewer for Synopsys CustomCompiler, text table format, simulation data
+
+## Before usage
+
+You will need:
+- Python 3
+- The bokeh ploting library (`pip install bokeh --user`)
+
+## Usage
+1. Edit the `wp.py` file configuration section to point to the data file, change the title, etc.
+2. In a terminal, execute `python3 wv.py` or `./we.py` (if you changed the file to be executable).
+
+## TODO
+- Use pandas dataframe to manipulate the data.
--- wv.py
+++ wv.py
@@ -1,15 +1,33 @@
 #!/usr/bin/python3
 
+import argparse
+import os
 import re
 from bokeh.plotting import figure, output_file, show
 
+parser = argparse.ArgumentParser(description = "Creates an HTML file containing the plot of waves defined by the input file in text table format.")
+parser.add_argument("-i", "--input_file", type = str, dest = "wave_input_file", required = True, help = "Specifies the input waveform file in text table format.")
+parser.add_argument("-o", "--output_file", type = str, dest = "wave_output_file", help = "Specifies the output plot file.")
+parser.add_argument("-t", "--title", type = str, dest = "title", help = "Specifies the output plot title.")
+parser.add_argument("-dp", "--show_data_points", action = "store_true", help = "Show data point over the plot-line.")
+args = parser.parse_args()
+
 # Data configutation section ##################################################
 
-wave_file = "/home/mvico/Documentos/DACI_2018/practico/TP_1/imagenes/gmID_vs_ID/gmID_vs_ID"
-plot_title = "gmID_vs_ID"
+wave_input_file = args.wave_input_file
+
+if (args.wave_output_file.endswith(".html")):
+    wave_output_file = args.wave_output_file
+else:
+    wave_output_file = args.wave_output_file + ".html"
+
+if (args.title == None):
+    _, plot_title = os.path.split(args.wave_input_file)
+else:
+    plot_title = args.title
 
 #show_data_points = False
-show_data_points = True
+#show_data_points = True
 
 # Plot configutation section ##################################################
 
@@ -24,7 +42,7 @@ line_index = 0
 x = []
 y = []
 
-with open(wave_file) as file:
+with open(wave_input_file) as file:
     for line in file:
         if (line_index > header_lines - 1):
             match = re.findall(r"\s*(\d+\.\d+E[+|-]\d+)\s*", line)
@@ -35,13 +53,12 @@ with open(wave_file) as file:
 x = [float(x) for x in x]
 y = [float(y) for y in y]
 
-output_file("{}.html".format(plot_title))
-print("{}.html".format(plot_title))
+output_file("{}.html".format(wave_output_file))
 p = figure(title = plot_title)
 
 p.line(x, y, legend = plot_title, line_width = 2, color = data_line_color)
 
-if (show_data_points):
+if (args.show_data_points):
     p.circle(x, y, color = data_points_color)
 
 show(p)

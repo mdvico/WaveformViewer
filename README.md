edfb2855077badbec2c03657770fc0528ee32b36
Matías Danel, Vico <matias.daniel.vico@gmail.com>
2018-10-07 18:53:25 -0300

    Included pandas support. Included support for multiple plots. Several improvements in internal data handling. Added some input arguments to give the user more control over the output plot.

--- README.md
+++ README.md
@@ -5,11 +5,15 @@ Waveform viewer for Synopsys CustomCompiler, text table format, simulation data
 
 You will need:
 - Python 3
-- The bokeh ploting library (`pip install bokeh --user`)
+- The `bokeh` ploting library (`pip install bokeh --user`)
+- The `pandas` library (`pip install pandas --user`)
 
 ## Usage
 1. Edit the `wp.py` file configuration section to point to the data file, change the title, etc.
-2. In a terminal, execute `python3 wv.py` or `./we.py` (if you changed the file to be executable).
+2. In a terminal, execute `python3 wv.py` or `./wv.py` (if you changed the file to be executable).
 
 ## TODO
 - Use pandas dataframe to manipulate the data.
+- Include support for multiple waveforms.
+- Add support for batch file processing.
+- Add argument to avoid showing the output, just to create the output file.
--- wv.py
+++ wv.py
@@ -3,13 +3,16 @@
 import argparse
 import os
 import re
+import pandas as pd
 from bokeh.plotting import figure, output_file, show
+from bokeh.colors import named
 
 parser = argparse.ArgumentParser(description = "Creates an HTML file containing the plot of waves defined by the input file in text table format.")
 parser.add_argument("-i", "--input_file", type = str, dest = "wave_input_file", required = True, help = "Specifies the input waveform file in text table format.")
 parser.add_argument("-o", "--output_file", type = str, dest = "wave_output_file", help = "Specifies the output plot file.")
 parser.add_argument("-t", "--title", type = str, dest = "title", help = "Specifies the output plot title.")
 parser.add_argument("-dp", "--show_data_points", action = "store_true", help = "Show data point over the plot-line.")
+parser.add_argument("-lw", "--line_width", type = int, dest = "line_width", choices = range(1,7), default = 1, help = "Specifies the data line width. Affects all the lines.")
 args = parser.parse_args()
 
 # Data configutation section ##################################################
@@ -26,39 +29,51 @@ if (args.title == None):
 else:
     plot_title = args.title
 
+line_width = args.line_width
 #show_data_points = False
 #show_data_points = True
 
 # Plot configutation section ##################################################
 
-data_line_color = "red"
-data_points_color = "blue"
+#data_line_color = "red"
+#data_points_color = "blue"
+#color_index = 0
+#colors = [name for name in named.__all__]
+colors = ["red", "blue", "green", "yellow", "purple", "black"]
 
 ###############################################################################
+
+# Example of a text table
+# #format table ## [Custom WaveView] saved 14:46:01 Sat Oct  6 2018
+# XVAL gm/id_long_L_6_:_dc id_long_6
+#  0.000E+00  1.986E+01 -0.000E+00
 
 header_lines = 2
 line_index = 0
 
-x = []
-y = []
+match = []
 
 with open(wave_input_file) as file:
     for line in file:
-        if (line_index > header_lines - 1):
-            match = re.findall(r"\s*(\d+\.\d+E[+|-]\d+)\s*", line)
-            x.append(match[0])
-            y.append(match[1])
+        if (line_index >= header_lines):
+            match.append([float(num) for num in line.strip().split()])
+        elif (line_index == 1):
+            #signals_list = re.split(r"XVAL (.*?)\:", line)[1::]
+            #signals_list.insert(0, "x_axis")
+            signals_list = line.split()
+            print(signals_list)
         line_index += 1
 
-x = [float(x) for x in x]
-y = [float(y) for y in y]
+table = pd.DataFrame(columns = signals_list, data = match)
+print(table)
 
 output_file("{}.html".format(wave_output_file))
 p = figure(title = plot_title)
 
-p.line(x, y, legend = plot_title, line_width = 2, color = data_line_color)
-
-if (args.show_data_points):
-    p.circle(x, y, color = data_points_color)
+for signal, data_color in zip(signals_list[1::], colors):
+    p.line(table[signals_list[0]], table[signal], legend = signal, line_width = line_width, color = data_color)
+    #color_index += 1
+    if (args.show_data_points):
+        p.circle(table[signals_list[0]], table[signal], color = data_color)
 
 show(p)

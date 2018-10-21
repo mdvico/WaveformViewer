22a374dca1b2d7533d900c04c591e066a4465bb9
Matías Danel, Vico <matias.daniel.vico@gmail.com>
2018-10-21 11:03:48 -0300

    Added arguments for exporting SVG and PNG plot files. Added new argument to avoid creating the HTML output file and showing it in the browser.

--- README.md
+++ README.md
@@ -4,8 +4,14 @@ Waveform viewer for Synopsys CustomCompiler, text table format, simulation data
 ## Before usage
 You will need:
 - Python 3
-- The `bokeh` ploting library (`pip install bokeh --user`)
-- The `pandas` library (`pip install pandas --user`)
+- The `bokeh` ploting library (`pip3 install bokeh --user`)
+- The `pandas` library (`pip3 install pandas --user`)
+- The `pillow` library for PNG export (`pip3 install pillow --user`)
+- The `selenium` library for PNG export (`pip3 install selenium --user`)
+
+If you have problems related to `phantomjs` try the following:
+- Install npm (`apt install npm`)
+- Install phantomjs-prebuilt (npm install phantomjs-prebuilt)
 
 ## Usage
 1. Edit the `wp.py` file configuration section to point to the data file, change the title, etc.
@@ -13,18 +19,20 @@ You will need:
 
 ## TODO
 - [ ] Add support for batch file processing.
-- [ ] Add argument to avoid showing the output, just to create the output file.
 - [ ] Add option to export table.
 - [ ] Change x axis notation to scientific (milli, micro, nano, etc.).
 - [ ] Add some sort of verbosity.
 - [ ] Add some sort of debug capability.
 - [ ] Add the option of sourcing a local configuration file to have a consistent format for different plots.
 - [ ] Add argument to change the axis labels.
-- [ ] Add argument to ask for an PNG output file.
 - [ ] Add option to control the DPI output for the PNG output file.
+- [ ] Add argument to control the axis ticks, log, db20, etc.
 
 ## DONE
 - [x] Use pandas dataframe to manipulate the data.
 - [x] Include support for multiple waveforms.
 - [x] Allow to mute the different plots pressing on its legends.
 - [x] Avoid showing the pandas dataframe on the terminal.
+- [x] Add argument to ask for an PNG output file.
+- [x] Add argument to ask for an SVG output file.
+- [x] Add argument to avoid showing the output, just to create the output file.
--- wv.py
+++ wv.py
@@ -6,23 +6,24 @@ import re
 import pandas as pd
 from bokeh.plotting import figure, output_file, show
 from bokeh.colors import named
+from bokeh.io import export_png
+from bokeh.io import export_svgs
 
 parser = argparse.ArgumentParser(description = "Creates an HTML file containing the plot of waves defined by the input file in text table format.")
 parser.add_argument("-i", "--input_file", type = str, dest = "wave_input_file", required = True, help = "Specifies the input waveform file in text table format.")
 parser.add_argument("-o", "--output_file", type = str, dest = "wave_output_file", help = "Specifies the output plot file.")
 parser.add_argument("-t", "--title", type = str, dest = "title", help = "Specifies the output plot title.")
-parser.add_argument("-dp", "--show_data_points", action = "store_true", help = "Show data point over the plot-line.")
+parser.add_argument("-dp", "--show_data_points", action = "store_true", default = False, help = "Show data point over the plot-line.")
 parser.add_argument("-lw", "--line_width", type = int, dest = "line_width", choices = range(1,7), default = 1, help = "Specifies the data line width. Affects all the lines.")
+parser.add_argument("--save_svg", action = "store_true", default = False, help = "Saves the plot in a svg file.")
+parser.add_argument("--save_png", action = "store_true", default = False, help = "Saves the plot in a png file.")
+parser.add_argument("--no_output", action = "store_true", default = False, help = "Does not create the HTML output file and does not open the browser to show it.")
 args = parser.parse_args()
 
 # Data configutation section ##################################################
 
 wave_input_file = args.wave_input_file
-
-if (args.wave_output_file.endswith(".html")):
-    wave_output_file = args.wave_output_file
-else:
-    wave_output_file = args.wave_output_file + ".html"
+wave_output_file = args.wave_output_file
 
 if (args.title == None):
     _, plot_title = os.path.split(args.wave_input_file)
@@ -58,7 +59,7 @@ with open(wave_input_file) as file:
 
 table = pd.DataFrame(columns = signals_list, data = match)
 
-output_file(wave_output_file)
+output_file(wave_output_file + ".html")
 p = figure(title = plot_title)
 
 for signal, data_color in zip(signals_list[1::], colors):
@@ -67,4 +68,13 @@ for signal, data_color in zip(signals_list[1::], colors):
         p.circle(table[signals_list[0]], table[signal], color = data_color, alpha = 0.4, muted_color = data_color, muted_alpha = 0.2)
 
 p.legend.click_policy = "mute"
-show(p)
+
+if (args.save_png):
+    p.toolbar_location = None
+    export_png(p, filename = wave_output_file + ".png")
+    p.toolbar_location = "right"
+if (args.save_svg):
+    p.output_backend = "svg"
+    export_svgs(p, filename = wave_output_file + ".svg")
+if (not args.no_output):
+    show(p)

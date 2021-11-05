6f92118a316a9d399e96906d2cd78154d933377d
mvico <matias.daniel.vico@gmail.com>
2021-11-05 10:59:22 -0300

    Saving figures on files with 300 DPI and solid background now

--- waveform/wave_io.py
+++ waveform/wave_io.py
@@ -3,9 +3,8 @@ import pandas as pd
 
 def load(wave_input_file, header, sep):
     return pd.read_csv(wave_input_file, header=header, sep=sep,
-            engine="python")
+                       engine="python")
 
 
 def save_figure(figure, filename, type="png"):
-    figure.savefig(f"{filename}.{type}")
-
+    figure.savefig(f"{filename}.{type}", dpi=300, transparent=False)
